b5889a7a7e4f2c10f05ffb6ba6a8929042e4d673
1001-1100 <joshuacapulemanzano@gmail.com>
2020-04-07 21:09:01 +0800

    added static url

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from rest_framework import routers                    
 from rest_framework_jwt.views import refresh_jwt_token
 from api import views                            
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()                      
 router.register(r'users', views.UserViewSet)
@@ -22,7 +24,7 @@ router.register(r'colleges', views.CollegeViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),                
-    path('api/init', views.init),                
+    path('api/init/', views.init),                
     path('api/generateschedule/', views.SchedulesList.as_view()),
     path('api/preferencelist/<int:pk>/', views.PreferenceList.as_view()),     
     path('api/courseprioritylist/<int:pk>/', views.CoursePriorityList.as_view()),     
@@ -31,4 +33,4 @@ urlpatterns = [
     path('api/auth/registration/', include('rest_auth.registration.urls')),                
     path('api/refresh-token/', refresh_jwt_token),
     path('', include('django.contrib.auth.urls')),                
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
