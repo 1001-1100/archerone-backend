2428cffdc618948a3afeaf96e606d72faac55498
1001-1100 <joshuacapulemanzano@gmail.com>
2020-04-14 18:53:06 +0800

    signals for notifs

--- api/signals.py
+++ api/signals.py
@@ -1,7 +1,8 @@
 from .models import User, Schedule, FriendRequest, Notification, Course, Degree, College, CoursePriority, Preference, Day, Faculty, Building, Section, CourseOffering, Timeslot, Room, FlowchartTerm
 
 def save_friend_request(sender, instance, **kwargs):
-    Notification(content=instance.from_user.first_name+' accepted your friend request!', seen=False, to_user=instance.to_user).save()
+    if(instance.accepted):
+        Notification(content=instance.from_user.first_name+' accepted your friend request!', seen=False, to_user=instance.to_user).save()
 
 def save_schedule(sender, instance, **kwargs):
     for u in instance.user.friends.all():
