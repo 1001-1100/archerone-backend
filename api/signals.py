edbe7df8d38afae192ed6d304deff672685becab
1001-1100 <joshuacapulemanzano@gmail.com>
2020-04-14 23:14:52 +0800

    signals

--- api/signals.py
+++ api/signals.py
@@ -4,6 +4,14 @@ def save_friend_request(sender, instance, **kwargs):
     if(instance.accepted):
         Notification(content=instance.to_user.first_name+' accepted your friend request!', seen=False, to_user=instance.from_user).save()
 
-def save_schedule(sender, instance, **kwargs):
-    for u in instance.user.friends.all():
-        Notification(content=instance.user.first_name+' saved a new schedule named \''+instance.title+'\'!', seen=False, to_user=u).save()
+def save_schedule(sender, instance, created, **kwargs):
+    if(created):
+        for u in instance.user.friends.all():
+            Notification(content=instance.user.first_name+' saved a new schedule named \''+instance.title+'\'!', seen=False, to_user=u).save()
+    else:
+        for u in instance.user.friends.all():
+            Notification(content=instance.user.first_name+' modified the schedule named \''+instance.title+'\'!', seen=False, to_user=u).save()
+
+# def save_preference(sender, instance, **kwargs):
+#     if(instance.accepted):
+#         Notification(content=instance.to_user.first_name+' accepted your friend request!', seen=False, to_user=instance.from_user).save()
