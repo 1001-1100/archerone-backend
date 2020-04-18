ea94639cf7b0922f2dc08ac1434a0c2bd7959d40
1001-1100 <joshuacapulemanzano@gmail.com>
2020-04-18 23:13:12 +0800

    course offering creation

--- api/views.py
+++ api/views.py
@@ -314,6 +314,7 @@ def init(request):
                     if(current_enrolled > max_enrolled):
                       current_enrolled = max_enrolled
                     status = True
+                    CourseOffering.objects.get_or_create(classnumber=classnumber, faculty=faculty, course=course, section=section, day=day, timeslot=timeslot,room=room, status=status)
                     offerings = CourseOffering.objects.filter(classnumber=classnumber, faculty=faculty, course=course, section=section, day=day, timeslot=timeslot,room=room, status=status)
                     for o in offerings:
                       o.current_enrolled = current_enrolled
