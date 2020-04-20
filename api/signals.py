2425f8696b4f21d32a139bad350b63a92a6d063a
1001-1100 <joshuacapulemanzano@gmail.com>
2020-04-20 23:23:08 +0800

    user default preferences

--- api/signals.py
+++ api/signals.py
@@ -15,6 +15,11 @@ def save_schedule(sender, instance, created, **kwargs):
         for u in instance.user.friends.all().exclude(id=instance.user.id):
             Notification(category='Schedule', content=instance.user.first_name+' modified the schedule named \''+instance.title+'\'!', seen=False, to_user=u).save()
 
+def save_user(sender, instance, created, **kwargs):
+    if(created):
+        user_id = instance.id
+        Preference(user=user_id, earliest_class_time='7:30', latest_class_time='21:00', break_length=15, min_courses=0, max_courses=10).save()
+
 # def save_preference(sender, instance, **kwargs):
 #     if(instance.accepted):
 #         Notification(content=instance.to_user.first_name+' accepted your friend request!', seen=False, to_user=instance.from_user).save()
