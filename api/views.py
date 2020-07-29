602b08532a88bd2e0d6e3133e69e40cded456923
1001-1100 <joshuacapulemanzano@gmail.com>
2020-07-29 22:14:18 +0800

    courseinfo

--- api/views.py
+++ api/views.py
@@ -166,11 +166,11 @@ class CourseInfo(APIView):
       soft_pre = []
       co_req = []
       for c in course.prerequisite_to.all():
-        prereq.append(c.course_code)
+        prereq.append(Course.objects.get(id=c).course_code)
       for c in course.soft_prerequisite_to.all():
-        soft_pre.append(c.course_code)
+        soft_pre.append(Course.objects.get(id=c).course_code)
       for c in course.co_requisite.all():
-        co_req.append(c.course_code)
+        co_req.append(Course.objects.get(id=c).course_code)
       serializer = CourseSerializer(course)
       for d in serializer.data:
         d['prerequisite_to'] = prereq
