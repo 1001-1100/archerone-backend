e18923869a7364bc02fd31ed29d400b10657e9f2
1001-1100 <joshuacapulemanzano@gmail.com>
2020-08-18 15:06:29 +0800

    sharecode

--- api/views.py
+++ api/views.py
@@ -529,11 +529,7 @@ class SchedulesListFriends(APIView):
 
     allUsers.sort(key=lambda x: x['user'], reverse=True)
 
-    def random_string_generator(str_size, allowed_chars):
-      return ''.join(random.choice(allowed_chars) for x in range(str_size))
-    chars = string.ascii_letters 
-    size = 6 
-    shareCode = random_string_generator(size,chars) 
+    shareCode = str(random.randint(10000000, 99999999))
 
     if(True):
       serializedSchedules = []
