c4c78bc0b61730bf60cf9ca5712113b33982b22f
1001-1100 <joshuacapulemanzano@gmail.com>
2020-09-02 00:25:35 +0800

    google login

--- backend/urls.py
+++ backend/urls.py
@@ -75,5 +75,5 @@ urlpatterns = [
     path('api/refresh-token/', refresh_jwt_token),
     # path('', include('django.contrib.auth.urls')),                
     path('accounts/', include('allauth.urls')),                
-    path('admin/', include('admin.site.urls')),                
+    path('admin/', admin.site.urls),                
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
