f9c990bcd74a548f6a63e06c7e409d1bed037534
1001-1100 <joshuacapulemanzano@gmail.com>
2020-09-10 15:38:53 +0800

    email end

--- backend/settings.py
+++ backend/settings.py
@@ -54,20 +54,13 @@ INSTALLED_APPS = [
 ]
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-if(IS_LOCAL):
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST_USER = 'archerone.testing@gmail.com'
-    EMAIL_HOST_PASSWORD = 'ARCHER1:D'
-    DEFAULT_FROM_EMAIL = 'archerone.testing@gmail.com'
-else:
-    EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
-    EMAIL_HOST = 'smtp.sendgrid.net'
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
-    EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'archerone.testing@gmail.com'
+EMAIL_HOST_PASSWORD = 'ARCHER1:D'
+DEFAULT_FROM_EMAIL = 'archerone.testing@gmail.com'
 
 ACCOUNT_EMAIL_SUBJECT_PREFIX = '[AnimoSched] '
 EMAIL_SUBJECT_PREFIX = '[AnimoSched] '
