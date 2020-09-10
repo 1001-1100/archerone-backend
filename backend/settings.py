f64eed11a408069f76230f68e09cb4d1ddc27b48
1001-1100 <joshuacapulemanzano@gmail.com>
2020-09-10 15:40:19 +0800

    database

--- backend/settings.py
+++ backend/settings.py
@@ -104,17 +104,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if(IS_LOCAL):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True) 
-    }
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True) 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
